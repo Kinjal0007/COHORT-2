2f3fd8f99eecd00145a9c2db9518292fd4b3e97f
Mr_Wick <blackpanther.bp007@gmail.com>
2024-01-11 03:02:36 +0530

    Solved 1st week's Easy Assignments

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
+  str1=str1.toLowerCase();
+  str2=str2.toLowerCase();
+  let a=str1.split('').sort().join('');
+  let b=str2.split('').sort().join('');
 
+  if(a.length!=b.length){
+    return false;
+  }
+
+  for (let i = 0; i < str1.length; i++) {
+    if(a[i]!=b[i]){
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -13,8 +13,23 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+function calculateTotalSpentByCategory(transactions){
+  const total_amt={};
+  transactions.forEach(transaction => {
+    const {category,price}=transaction;
+    if(!total_amt[category]){
+      total_amt[category]=price;
+    }
+    else{
+      total_amt[category]+=price;
+    }
+  });
+
+  const resultant=[];
+  Object.keys(total_amt).forEach((category) => {
+    resultant.push({category: category,totalSpent: total_amt[category]});
+  });
+  return resultant;
 }
 
 module.exports = calculateTotalSpentByCategory;
